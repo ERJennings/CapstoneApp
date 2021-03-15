2f35cdf6768aa8e64c68acea783ebe2495ad420f
ERJennings <erjennings13@gmail.com>
2021-03-15 02:31:16 -0400

    Made buttons work for demonstration purposes

--- app/src/main/java/com/example/discsync/MainActivity.java
+++ app/src/main/java/com/example/discsync/MainActivity.java
@@ -3,17 +3,47 @@ package com.example.discsync;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button createButton;
+    Button joinButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createButton = (Button) findViewById(R.id.create);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateActivity();
+            }
+        });
+
+        joinButton = (Button) findViewById(R.id.join);
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openJoinActivity();
+            }
+        });
+
         //Toolbar myToolbar = findViewById(R.id.toolbar);
         //setSupportActionBar(myToolbar);
         //getSupportActionBar().setDisplayShowTitleEnabled(true);
+    }
+    public void openCreateActivity(){
+        Intent intent = new Intent(this, CreateActivity.class);
+        startActivity(intent);
+    }
+
+    public void openJoinActivity(){
+        Intent intent2 = new Intent(this, JoinActivity.class);
+        startActivity(intent2);
     }
 }
